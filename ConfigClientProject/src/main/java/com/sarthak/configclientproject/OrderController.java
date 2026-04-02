74dc11b62dd3f4f09cdcd90d9c6a596ec95abd00
sarthakpawar0912 <pawarsr06@gmail.com>
2026-04-02 10:39:03 +0530

    Initial commit - Central Config Server & Client

---
+++ ConfigClientProject/src/main/java/com/sarthak/configclientproject/OrderController.java
@@ -0,0 +1,17 @@
+package com.sarthak.configclientproject;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController {
+
+    @Value("${order.message}")
+    public String message;
+
+    @GetMapping("/order")
+    public String getOrderMessage() {
+        return message;
+    }
+}
